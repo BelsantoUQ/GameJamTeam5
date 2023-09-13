9f1df7caff000acf8d48c61175462fbba6b39e52
Santiago Velandia Gallo <santiago.velandiag@uqvirtual.edu.co>
2023-09-13 02:01:00 -0500

    Updated player controller script to include car destruction logic and visual effects for damage.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed; // Velocidad de movimiento lateral
     [SerializeField]
     private float jumpForce; // Fuerza de salto
+    float rotationSpeed; // Define la velocidad de cambio de rotación
+    
+    float minX = -5.0f; // Define límite del rango minimo de movimiento permitido
+    float maxX = 5.0f; // Define límite del rango maximo de movimiento permitido
     private Rigidbody rb;
     private float rotation;
     private bool isGrounded; // Variable para verificar si el jugador está en el suelo
@@ -18,25 +22,42 @@ public class PlayerController : MonoBehaviour
     private int hitsRemaining;
     private CarComponents carComponents;
     private bool noHit;
+    Color chasisColor;
+    private Material chasisMaterial;
+    private bool isDestroyed;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         planetController = FindObjectOfType<RotatePlanet>();
+        GameObject chasis = GameObject.FindGameObjectWithTag("Chasis");
+        Renderer chasisRenderer = chasis.GetComponent<Renderer>();
+        chasisMaterial = chasisRenderer.material;
+        chasisColor = chasisMaterial.color;
         jumpForce = 10f;
         moveSpeed = 5f;
         hitsRemaining = 2;
+        rotationSpeed = 2.0f;
         lateTunnelEffect = false;
         noHit = false;
+        isDestroyed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-        Jump();
+        if (isDestroyed)
+        {
+            planetController.SetSpeed(0f);
+        }
+        else
+        {
+            Move();
+            Jump();
+        }
     }
+
 
     private void Jump()
     {
@@ -55,9 +76,7 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float currentPosition = transform.position.x;
 
-        // Define los límites del rango permitido
-        float minX = -5.0f;
-        float maxX = 5.0f;
+        
 
         // Calcula la nueva posición después de aplicar la entrada horizontal
         float newPosition = currentPosition + (horizontalInput * moveSpeed * Time.deltaTime);
@@ -68,13 +87,11 @@ public class PlayerController : MonoBehaviour
         // Aplica la nueva posición
         transform.position = new Vector3(newPosition, transform.position.y, -12f);
 
-        // Limita la rotación vertical de la cámara dentro de los límites
+        // Limita la rotación vertical del carro dentro de los límites
         float newRotationX = transform.rotation.eulerAngles.x;
         newRotationX = Mathf.Clamp(newRotationX, -102.457f, -101f);
 
-        // Define la velocidad de cambio de rotación
-        float rotationSpeed = 2.0f;
-
+        
         // Calcula la nueva rotación
         float targetRotation = 0.0f;
 
@@ -138,7 +155,7 @@ public class PlayerController : MonoBehaviour
     
     public void SetNoHit(bool activate)
     {
-        this.activateTunnelEffect = activate;
+        this.noHit = activate;
         
     }
     
@@ -155,24 +172,66 @@ public class PlayerController : MonoBehaviour
         }
 
         // Verifica si hitsRemaining es mayor o igual a 1
-        if (hitsRemaining > 0)
+        if (hitsRemaining < 2 && !noHit)
         {
-            // Aplica el efecto de golpe en el objeto
-            // baja y sube sutilmente la opacidad del material del objeto para dar efecto de golpe
-            // Cambia el tono del albedo material del objeto a rojo de la misma manera que la opacidad
+            // Baja y sube sutilmente la opacidad
+            StartCoroutine(ChangeOpacity(chasisMaterial, 0.5f, 3));
 
-            // Sacude un poquito la cámara principal de la escena que está fuera de este gameObject
-            
+            // Cambia el tono del albedo material del objeto a rojo y vuelve al color original
+            StartCoroutine(ChangeAlbedoColor(chasisMaterial, Color.red, 3));
         }
-        else
-        {
-            // Si hitsRemaining es 0, desarma el carro
-            CarComponents[] carComponentsArray = FindObjectsOfType<CarComponents>();
 
-            foreach (CarComponents carComponent in carComponentsArray)
+        if (hitsRemaining >= 1) return;
+        // Si hitsRemaining es 0, desarma el carro
+        CarComponents[] carComponentsArray = FindObjectsOfType<CarComponents>();
+
+        foreach (CarComponents carComponent in carComponentsArray)
+        {
+            carComponent.SetRigidbody();
+        }
+
+        isDestroyed = true;
+    }
+
+    private IEnumerator ChangeOpacity(Material material, float targetOpacity, int numTimes)
+    {
+        for (int i = 0; i < numTimes; i++)
+        {
+            float currentOpacity = material.color.a;
+            float t = 0;
+            while (t < .5f)
             {
-                carComponent.SetRigidbody();
+                t += Time.deltaTime;
+                Color newColor = material.color;
+                newColor.a = Mathf.Lerp(currentOpacity, targetOpacity, t);
+                material.color = newColor;
+                yield return null;
             }
+            yield return new WaitForSeconds(0.1f); // Espera un momento antes de revertir el cambio de opacidad
+            // Invierte el objetivo de opacidad para crear un efecto de subida
+            targetOpacity = 1 - targetOpacity;
+        }
+    }
+
+    private IEnumerator ChangeAlbedoColor(Material material, Color targetColor, int numTimes)
+    {
+        Debug.Log("Color: :"+ material.color);
+        Color originalColor = material.color;
+        for (int i = 0; i < numTimes; i++)
+        {
+            float t = 0;
+            while (t < .2f)
+            {
+                t += Time.deltaTime;
+                Color newColor = Color.Lerp(originalColor, targetColor, t);
+                material.color = newColor;
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.1f); // Espera un momento antes de volver al color original
+            // Intercambia el color objetivo para volver al original
+            Color temp = originalColor;
+            originalColor = targetColor;
+            targetColor = temp;
         }
     }
 }
